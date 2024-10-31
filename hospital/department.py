c2da092266e7fd0532b316eb412700f22c59e877
adel ahmed <adelahmed123332100@gmail.com>
2024-10-31 22:49:44 +0300

    first commit

--- hospital/department.py
+++ hospital/department.py
@@ -1,1 +1,17 @@
-print("ZEYad")
+from hospital import hospital
+class department(hospital):
+    def __init__(self,name='',manger='',staff=[],patient=[]):
+        self.name = name
+        self.manger = manger
+        self.staff = staff
+        self.patient = patient
+    def __add_patient_to_dep(self,patient):
+        pass
+    
+    def __add_staff_to_deb(self,staff):
+        pass
+    def get_patient_in_dep():
+        pass
+    
+    def get_staff_in_deb(self):
+        pass
---
+++ hospital/hospital.py
@@ -0,0 +1,21 @@
+class hospital:
+    def __init__(self,name,location):
+        self.name = name
+        self.location = location
+        
+    
+    def add_dep():
+        pass
+    
+    def add_patient(self,patient,staff,dep):#to file
+        pass
+    
+    def add_staff(self,staff,dep):#to file
+        pass
+    
+    def get_all_staffs(self):
+        pass
+    
+    def get_all_patients(self):
+        pass
+    
