25501e9eb10b7e3de9766174d0f4c160e872da6a
zeyadtharwat66 <ziadtharwatsoliman@gmail.com>
2024-11-02 13:29:30 -0700

    done files

--- hospital/files.py
+++ hospital/files.py
@@ -14,19 +14,31 @@ class files():
     
     def file_to_dictionary(self):
         x = self.read_dic_location()
-        for i in x:
-            i.replace("{","").replace("}","")
-        dic = {}
+        for i in range(len(x)):
+            x[i]=x[i].replace("{","").replace("}","").replace("'",'')
+        dic1 = {}
+        dic2 = {}
         for i in x:
             if ':' in i:
                 key1 = i[0 : i.index(":")]
-                dic[key1] = (
+                dic1[key1] = (
+                    dic2# i[i.index(":")+1:]
+                )
+        print(x)
+        for i in range(len(x)):
+            x[i]=x[i][x[i].index(":")+1:].strip()
+        print(x)
+        for i in x:
+            if ':' in i:
+                key2 = i[0 : i.index(":")]
+                dic2[key2] = (
                     i[i.index(":")+1:]
                 )
-        return dic
+        return dic1
     
 x=files(r"C:\D\Amit\Duo\hospital\patient.txt")
 # x.write_to_file("patient.txt",{"zeyad":{"age":"22","phone_Number":"01148656665"},"adel":{"age":"21","phone_Number":"012"}})
 ziad=x.file_to_dictionary()
 print(ziad)
 print(type(ziad))
+print(type(ziad.get('zeyad')))
